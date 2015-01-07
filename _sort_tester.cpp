80812c3ee47b5daf27b59031b77bbc6a991e82c2
phoeagon <phoeagon@gmail.com>
2015-01-07 15:44:11 +0800

    quicksort

--- _sort_tester.cpp
+++ _sort_tester.cpp
@@ -1,14 +1,14 @@
 
 
 
-int test(void* ptr){
-	srand(time(NULL));
-	N = 1000000;
+int sort_tester(function<void(int*, int)> sorter){
+	int N = 1000000;
 	auto p = new int [N];
 	for (int i = 0 ; i < N; ++i)
 		p[i] = rand();
-	
+	sorter(p, N);
+	int ret = is_sorted(p, p+N);
+	delete [] p;
+	return ret;
 }
 
-int main() {
-}
--- quicksort.cpp
+++ quicksort.cpp
@@ -1,5 +1,5 @@
 #include "template.cpp"
-
+#include "_sort_tester.cpp"
 void _quicksort(int *data, int l, int r) {
 	int i = l , j = r;
 	int pivot = data[i];
@@ -13,10 +13,16 @@ void _quicksort(int *data, int l, int r) {
 			++i;	--j;
 		}
 	}
-	_quicksort(data, l, j);
-	_quicksort(data, i, r);
+	if (l<j)_quicksort(data, l, j);
+	if (i<r)_quicksort(data, i, r);
 }
 
 void quicksort(int *data, int N) {
 	_quicksort(data, 0, N-1);
+}
+
+int main(){
+	int T = 10;
+	while (T--)
+		assert(sort_tester(quicksort));
 }
