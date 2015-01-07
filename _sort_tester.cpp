141c1c066144b8bcfd90858cfed5b0e02735c9b6
phoeagon <phoeagon@gmail.com>
2015-01-07 14:19:37 +0800

    quick sort

---
+++ _sort_tester.cpp
@@ -0,0 +1,14 @@
+
+
+
+int test(void* ptr){
+	srand(time(NULL));
+	N = 1000000;
+	auto p = new int [N];
+	for (int i = 0 ; i < N; ++i)
+		p[i] = rand();
+	
+}
+
+int main() {
+}
--- quicksort.cpp
+++ quicksort.cpp
@@ -1,3 +1,22 @@
 #include "template.cpp"
 
-//TODO: implement this.
+void _quicksort(int *data, int l, int r) {
+	int i = l , j = r;
+	int pivot = data[i];
+	while ( i <= j ) {
+		while(data[i] < pivot) ++ i;
+		while(data[j] > pivot) -- j;
+		if ( i <= j ) {
+			auto x = data[i];
+			data[i] = data[j];
+			data[j] = x;
+			++i;	--j;
+		}
+	}
+	_quicksort(data, l, j);
+	_quicksort(data, i, r);
+}
+
+void quicksort(int *data, int N) {
+	_quicksort(data, 0, N-1);
+}
